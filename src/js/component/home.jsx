6e3ce7020784262b3828e226493a918a26005a98
Jose David Caro Cantor <43614556+josecaro02@users.noreply.github.com>
2024-02-12 19:48:45 +0000

    [useState explanation] - home.jsx: use two useStates to create a like and dislike buttons - index.js: send a prop to home.jsx for testing

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,39 @@
-import React from "react";
+import React, {useState} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+const Home = ({saludo}) => {
+	// aqui va mi logica
+	// useState esta pendiente del cambio de una variable
+	// y si cambia la variable se recarga el componente
+	//  const[ variable, funcion que cambia la variable] = useState(valor inicial de la variable)
+	// comillas ``
+	const [likes, setLikes] = useState(0)
+	const [isLike, setIsLike] = useState(false)
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className={`text-center ${isLike == false ? "bg-warning" : "bg-info"}`}>
+			{saludo}
+			<h1>Likes {likes}</h1>
+			{/* Si isLike es false aparece el boton verde de dar like
+				Si isLike es true aparece  el boton rojo de dar dislike*/}
+			{
+				isLike == false 
+				? 
+				<button className="btn btn-success" onClick={()=>{
+					setLikes(likes + 1)
+					setIsLike(true)
+				}}>Like</button>
+				:
+				<button className="btn btn-danger" onClick={()=>{
+					setLikes(likes - 1)
+					setIsLike(false)
+				}}>Dislike</button>
+			}
+
+			
+			
 		</div>
 	);
 };
